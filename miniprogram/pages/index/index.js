1e1a83494638a0b9dc76ad68ac600767fb54f3e5
Lujingxia9284 <lujingxia9284@gmail.com>
2019-05-21 13:24:38 +0800

    添加转发

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,6 +1,8 @@
 //index.js
 const app = getApp()
 var util=require('../../util/util.js')
+const db = wx.cloud.database()
+const userInfo = db.collection('userInfo')
 
 
 Page({
@@ -18,16 +20,13 @@ Page({
       { path: '../../image/outdoor.png', name: '外出', width: 64, height: 62 }
     ],
     left: 180,
-    Users: [
-      {}, {}
-    ],
+    Users: [{}, {}],
     userInfo: {
-      avatarUrl: "",//用户头像
+      avatarUrl: "eee",//用户头像
       nickName: "",//用户昵称
     },
     hasUserInfo: false,
-    // -------
-    //
+   
     // ----实现状态滑动---
     changeX: app.globalData.changeX,
     lastchangeX: 0,
@@ -38,7 +37,7 @@ Page({
       { path: '../../image/fun.png', name: '娱乐', width: 64, height: 62 },
       { path: '../../image/outdoor.png', name: '外出', width: 64, height: 62 }
     ],
-    // -------------------
+  
 
     // ----公告栏和模态框-----
     hideMod: true,
@@ -50,13 +49,21 @@ Page({
     Index: '',
   },
 
+
+
+
+
+
+
+
+
+
+
   ChangeState: function (e) {
     var left = this.left;
     var clientX = e.touches[0].clientX;
 
-    // Users:[
-    // {},{}
-    // ],
+    
 
     // ----获取用户-----
    
@@ -69,9 +76,7 @@ Page({
     this.setData({ left: left });
 
   },
-  TopublicBill: function () {
 
-  },
   
   addAnnoucement:function(){
     var AnnouceMent=this.data.AnnouceMent
@@ -89,9 +94,8 @@ Page({
 
   },
 
-
+//------------------页面路由-----------------------
   TopublicBill:function(){
-
     wx.navigateTo({
       url: '../../pages/publicBill/publicBill',
     })
@@ -100,82 +104,38 @@ Page({
     wx.navigateTo({
       url: '../../pages/dutyRoster/dutyRoster',
     })
-
   },
 
+  // 滑动状态栏
 
-
-// 登陆
-
-
-  onLoad: function (options) {
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-
-// 滑动状态栏
-
-  bindchagehandle:function(e){
+  bindchagehandle: function (e) {
     // console.log(e.detail.x)
-    var lastchangeX=this.data.lastchangeX
+    var lastchangeX = this.data.lastchangeX
     this.setData({
-      lastchangeX :e.detail.x
+      lastchangeX: e.detail.x
     })
 
   },
-  bindtouchendhandle:function(e){
+  bindtouchendhandle: function (e) {
     console.log('需要传到后台保存状态')
     this.setData({
-     wholeWidth: wx.getSystemInfoSync().windowWidth
-   })
-    var propotion=this.data.wholeWidth/375
+      wholeWidth: wx.getSystemInfoSync().windowWidth
+    })
+    var propotion = this.data.wholeWidth / 375
 
-    var changeX=this.data.changeX
-    var lastchangeX=this.data.lastchangeX
-    if(lastchangeX<56*propotion){
-      lastchangeX=31*propotion
-    }else if(lastchangeX>=56*propotion&&lastchangeX<125*propotion){
-      lastchangeX=95*propotion
-    }else if(lastchangeX>=125*propotion&&lastchangeX<195*propotion){
-      lastchangeX=163*propotion
-    }else if(lastchangeX>=195*propotion){
-      lastchangeX=231*propotion
+    var changeX = this.data.changeX
+    var lastchangeX = this.data.lastchangeX
+    if (lastchangeX < 56 * propotion) {
+      lastchangeX = 31 * propotion
+    } else if (lastchangeX >= 56 * propotion && lastchangeX < 125 * propotion) {
+      lastchangeX = 95 * propotion
+    } else if (lastchangeX >= 125 * propotion && lastchangeX < 195 * propotion) {
+      lastchangeX = 163 * propotion
+    } else if (lastchangeX >= 195 * propotion) {
+      lastchangeX = 231 * propotion
     }
     this.setData({
-      changeX : lastchangeX
+      changeX: lastchangeX
     })
   },
   //
@@ -183,32 +143,32 @@ Page({
 
 
   // 模态框
-  showModal:function(e) {
+  showModal: function (e) {
     console.log('展示模态框')
     this.setData({
-      hideMod : false,
-      disableScroll:true,
-      Index:e.currentTarget.dataset.index
+      hideMod: false,
+      disableScroll: true,
+      Index: e.currentTarget.dataset.index
     })
   },
-  hideModal:function(e) {
+  hideModal: function (e) {
     console.log('隐藏模态框')
     this.setData({
-      hideMod:true
+      hideMod: true
     })
   },
 
-  Modalhandle:function(e){
+  Modalhandle: function (e) {
     console.log('输入的同时改变公告栏的内容')
-    var param={}
+    var param = {}
     console.log(e.detail.value)
-    var annoucement=this.data.AnnouceMent
-    var string = "AnnouceMent["+this.data.Index+"].content";
-    param[string]=e.detail.value
+    var annoucement = this.data.AnnouceMent
+    var string = "AnnouceMent[" + this.data.Index + "].content";
+    param[string] = e.detail.value
     this.setData(param)
   },
 
-// 实现左滑
+  // 实现左滑
   ListTouchStart(e) {
     this.setData({
       ListTouchStart: e.touches[0].pageX
@@ -237,14 +197,128 @@ Page({
       ListTouchDirection: null
     })
   },
-  DeleteHandle:function(e){
-    var AnnouceMent=this.data.AnnouceMent
+  DeleteHandle: function (e) {
+    var AnnouceMent = this.data.AnnouceMent
 
-    AnnouceMent.splice(e.currentTarget.dataset.index,1)
+    AnnouceMent.splice(e.currentTarget.dataset.index, 1)
     this.setData({
-      AnnouceMent : AnnouceMent,
+      AnnouceMent: AnnouceMent,
     })
-  }
+  },
+
+  //------分享给好友---------
+  onShareAppMessage: function () {
+    var that = this;
+
+    return {
+      title: '舍佐',
+      path: '/pages/index/index?avatarUrl=' + that.data.userInfo.avatarUrl,
+     
+     }
+  },
+
+
+// 登陆
+
+
+  onLoad: function (options) {
+    var that=this;
+    console.log(that.data.userInfo.avatarUrl)
+    if (options.avatarUrl){
+      that.setData({
+      shareUrl: options.avatarUrl
+      })
+      }
+      else{
+      that.setData({
+        shareUrl: "hhh"
+      })
+    }
+    db.collection('userInfo').doc('woshizhu').set({
+      data: {
+        description: 'share',
+        url: that.data.shareUrl
+      }
+    }).then(res => {
+      console.log(res)
+    })
+    
+  
+    console.log(that.data.shareUrl);
+
+    db.collection('userInfo').doc('wozenmehaimeixiehao').set({
+      data:{
+        description: 'my',
+        url: that.data.userInfo.avatarUrl 
+        
+       }
+    }).then(res => {
+      console.log(res)
+    })
+
+
+    db.collection('userInfo').doc('woshizhu').get().then(res => {
+      // res.data 包含该记录的数据
+      that.setData({ aaa: res.data.url})
+      console.log(res.data.url)
+    })
+
+
+    db.collection('userInfo').doc('wozenmehaimeixiehao').get().then(res => {
+      // res.data 包含该记录的数据
+      that.setData({ bbb: res.data.url})
+      console.log(res.data.url)
+    })
+
+  
+
+
+
+
+
+    wx.showShareMenu({
+      withShareTicket: true
+    });
+
+
+
+
+    if (app.globalData.userInfo) {
+      that.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (that.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        that.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          that.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
 
 
 
